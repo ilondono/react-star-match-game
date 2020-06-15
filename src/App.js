24430a513fe751a432d09ec9bbd38a5b91f5cb90
ivandlondono <ivandlondono@gmail.com>
2020-06-15 16:33:55 -0500

    - Extracting utilities objects to an independent file so it can be used from different components. - Extracting the functionality of showing the stars to a new component: StarsDisplay.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
+import utils from './utilities.js';
 import PlayNumber from './components/PlayNumber.js';
-
- // Color Theme
- const colors = {
-  available: 'lightgray',
-  used: 'lightgreen',
-  wrong: 'lightcoral',
-  candidate: 'deepskyblue',
-};
-
-// Math science
-const utils = {
-  // Sum an array
-  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
-
-  // create an array of numbers between min and max (edges included)
-  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
-
-  // pick a random number between min and max (edges included)
-  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
-
-  // Given an array of numbers and a max...
-  // Pick a random sum (< max) from the set of all available sums in arr
-  randomSumIn: (arr, max) => {
-    const sets = [[]];
-    const sums = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0, len = sets.length; j < len; j++) {
-        const candidateSet = sets[j].concat(arr[i]);
-        const candidateSum = utils.sum(candidateSet);
-        if (candidateSum <= max) {
-          sets.push(candidateSet);
-          sums.push(candidateSum);
-        }
-      }
-    }
-    return sums[utils.random(0, sums.length - 1)];
-  },
-};
+import StarsDisplay from './components/StarsDisplay.js'
 
 const App = () => {
 
@@ -53,7 +17,7 @@ const App = () => {
 
       <div className="body">
         <div className="left">
-          {utils.range(1, stars).map(starId => <div key={starId} className="star"/>)}
+          <StarsDisplay count={stars}/>
         </div>
 
         <div className="right">
